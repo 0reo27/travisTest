098e5754c16c94f1c418f3fcfc6924626a8df7e6
0reo27 <stopspammingme@hotmail.ph>
2018-04-28 15:08:55 +0800

    q

--- buildMagisk.sh
+++ buildMagisk.sh
@@ -4,7 +4,7 @@ git config --global user.name "John Appleseed"
 git config --global user.email "email@example.com"
 rm -rf Magisk
 repo init -u https://github.com/0reo27/travisTest --depth=1
-repo sync
+repo sync --force-sync
 cp -f release_signature.jks $HOME/Magisk
 cd $HOME/Magisk
-time python3.6 build.py --release all 16.4 1640 > result.txt
+time python3.6 build.py --release all 16.4 1640
